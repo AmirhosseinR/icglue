21fef3835a34724e73563c0b3d3cbbdb62998464
Amirhossein Rostami <a.rostami88@gmail.com>
2026-07-31 13:55:01 +0000

    Update sample

--- schematic/sample.v
+++ schematic/sample.v
@@ -32,21 +32,6 @@ module axi2mem
    input  logic [AXI_USER_WIDTH-1:0] axi_slave_aw_user_i,
    output logic                      axi_slave_aw_ready_o,
 
-   // READ ADDRESS CHANNEL
-   input  logic                      axi_slave_ar_valid_i,
-   input  logic [AXI_ADDR_WIDTH-1:0] axi_slave_ar_addr_i,
-   input  logic [2:0]                axi_slave_ar_prot_i,
-   input  logic [3:0]                axi_slave_ar_region_i,
-   input  logic [7:0]                axi_slave_ar_len_i,
-   input  logic [2:0]                axi_slave_ar_size_i,
-   input  logic [1:0]                axi_slave_ar_burst_i,
-   input  logic                      axi_slave_ar_lock_i,
-   input  logic [3:0]                axi_slave_ar_cache_i,
-   input  logic [3:0]                axi_slave_ar_qos_i,
-   input  logic [AXI_ID_WIDTH-1:0]   axi_slave_ar_id_i,
-   input  logic [AXI_USER_WIDTH-1:0] axi_slave_ar_user_i,
-   output logic                      axi_slave_ar_ready_o,
-
    // WRITE DATA CHANNEL
    input  logic                      axi_slave_w_valid_i,
    input  logic [AXI_DATA_WIDTH-1:0] axi_slave_w_data_i,
